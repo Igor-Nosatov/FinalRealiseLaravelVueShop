cd0bdc1b7f9613afc4843897a34f94c17c63a715
Igor-Nosatov <sinevor13@gmail.com>
2019-04-08 21:11:24 +0300

    add migration seeders controllers

---
+++ database/migrations/2019_04_08_174437_create_checkout_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCheckout extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkout', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('firstname')->nullable();
+          $table->string('lastname')->nullable();
+          $table->string('company')->nullable();
+          $table->string('phone')->nullable();
+          $table->string('email')->nullable();
+          $table->string('country')->nullable();
+          $table->string('adressone')->nullable();
+          $table->string('adresstwo')->nullable();
+          $table->string('city')->nullable();
+          $table->string('district')->nullable();
+          $table->string('postcode')->nullable();
+          $table->integer('total')->nullable();
+          $table->unsignedInteger('user_id')->nullable();
+          $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+          $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkout');
+    }
+}
---
+++ database/migrations/2019_04_08_174502_create_wishlist_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableWishlist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name')->nullable();
+          $table->string('image')->nullable();
+          $table->integer('price')->nullable();
+          $table->integer('qty')->nullable();
+          $table->unsignedInteger('user_id')->nullable();
+          $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist');
+    }
+}
